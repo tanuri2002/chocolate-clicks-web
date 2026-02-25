e64f22e3b90829f70a4fcc206d05a2917603c5bb
Tanuri Weerathunga <tanuribawanya@gmail.com>
2026-02-25 23:07:36 +0530

    fix: correct SignUp import case for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./frontend/Navbar";
 import Footer from "./frontend/Footer";
 import Home from "./frontend/Home";
 import Login from "./frontend/Login";
-import Signup from "./frontend/Signup";
+import SignUp from "./frontend/SignUp";
 import Payment from "./frontend/Payment";
 import Dashboard from './frontend/Dashboard';
 import FoodItems from "./frontend/FoodItems";
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/food" element={<FoodItems />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/events" element={<FoodItems />} />
